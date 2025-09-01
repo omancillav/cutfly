fd1b8225f4fda1421486f17441a870ef2d42db3e
omancillav <edgarmncllav04@gmail.com>
2025-09-01 11:02:51 -0600

    remove unused SVG files and update font imports in layout

---
+++ src/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Montserrat } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"], weight: ["400", "500", "600", "700"] });
+
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
