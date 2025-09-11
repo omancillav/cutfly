bf5c2f7fa3340a6d7d06fd17cde4f5194e0b558d
omancillav <edgarmncllav04@gmail.com>
2025-09-11 13:00:18 -0600

    feat: update font import to Saira for improved typography consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { sansation as font } from "@/ui/fonts";
+import { saira as font } from "@/ui/fonts";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { Analytics } from "@vercel/analytics/next";
