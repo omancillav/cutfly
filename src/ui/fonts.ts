d068626d64662a23e0fbdd8f96750f70f679a079
omancillav <edgarmncllav04@gmail.com>
2025-09-11 23:20:58 -0600

    feat: replace Saira font import with Geist for improved typography options

--- src/ui/fonts.ts
+++ src/ui/fonts.ts
@@ -1,11 +1,6 @@
-import { Inter, Play, Saira } from "next/font/google";
+import { Saira, Geist } from "next/font/google";
 
-export const inter = Inter({ subsets: ["latin"], weight: ["400", "500", "600", "700"] });
-
-export const play = Play({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-});
+export const geist = Geist({ subsets: ["latin"], weight: ["400", "500", "600", "700"] });
 
 export const saira = Saira({
   subsets: ["latin"],
